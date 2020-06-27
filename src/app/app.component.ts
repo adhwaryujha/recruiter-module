1a603d6d33b26cd7f3100f15c35a59ceacfbb976
Adhwaryu Jha <adhwaryujha@gmail.com>
2020-06-28 01:50:43 +0530

    added dummy variables and setters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Recruiter } from './models/recruiter';
+import { Answer } from './models/answer';
+import { Candidate } from './models/candidate';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'apli-frontend';
+
+    user: Recruiter;
+    answers: Answer[];
+    candidate: Candidate;
+
+    activeQuestionIndex: number;
+
+    constructor() {
+      this.fetchUserDetails();
+      this.fetchCandidateDetails;
+      this.fetchCandidateDetails;
+      this.activeQuestionIndex = 0;
+    }
+
+    fetchUserDetails(): void {
+      this.user =
+        { name: "Harvey Dent"
+        , imageURL: "../assets/recruiter.png"
+        };
+    }
+
+    fetchCandidateDetails(): void {
+      this.candidate =
+        { name: "Mike Ross"
+        , imageUrl: "../../assets/candidate.png"
+        };
+    }
+
+    fetchAnswers(): void {
+      this.answers = 
+        [
+          { videoURL: "", comment: "", rating: 0},
+          { videoURL: "", comment: "", rating: 0},
+          { videoURL: "", comment: "", rating: 0},
+          { videoURL: "", comment: "", rating: 0},
+          { videoURL: "", comment: "", rating: 0}
+        ];
+    }
+
+    changeActiveQuestion(newIndex: number): void {
+      this.activeQuestionIndex = newIndex;
+    }
+
+    
+
 }
